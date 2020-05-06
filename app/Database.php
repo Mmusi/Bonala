4b20937b39dc9d5cdb12718911235ad26411d96e
Thato Mmusi <mmusi2010@gmail.com>
2020-05-06 09:39:53 +0200

    Add files via upload

---
+++ app/Database.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BonalaPHP\App;
+
+/**
+ * Class Database Containing the Database informations
+ * @package BonalaPHP\App
+ */
+class Database extends \PDO{
+
+    /**
+     * Database constructor.
+     */
+    public function __construct() {
+		
+	
+		//values to your own database.
+		$_MYSQL_DB = 'blogMVC_OOP';
+		$_MYSQL_HOST = 'localhost'; 
+		$_MYSQL_USER = 'root';
+		$_MYSQL_PW = '';
+		
+		/*
+		define('MYSQL_DB', $_MYSQL_DB);
+		define('MYSQL_HOST', $_MYSQL_HOST);
+		define('MYSQL_USER', $_MYSQL_USER);
+		define('MYSQL_PW', $_MYSQL_PW);
+		*/
+		
+		
+		$dsn = 'mysql:dbname=' . $_MYSQL_DB . ';host=' . $_MYSQL_HOST;
+		$user = $_MYSQL_USER;
+		$pw = $_MYSQL_PW;
+		
+		try {
+			parent::__construct($dsn, $user, $pw);
+			$this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		}
+		catch(PDOException $e) {
+			echo 'Connection failed: ' . $e->getMessage();
+		}
+	}
+	
+}
---
+++ app/Router.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BonalaPHP\App;
+
+
+/**
+ * Class Router that is used to create routes dynamically.
+ * @package BonalaPHP\App
+ */
+
+class Router {
+    /**
+     * @param array $params contaning the two kind of routes we'll be using, p and act
+     */
+
+    public static function run (array $params) {
+		
+        $namespace = 'BonalaPHP\controller\\';
+        $controllerDef = $namespace . 'BonalaController';
+        $controllerPath = ROOT_PATH . 'controller/';
+        $controller = ucfirst($params['ctrl']);
+		
+        if (is_file($controllerPath . $controller . '.php')) {
+            $controller = $namespace . $controller;
+            $controllerNew = new $controller;
+			//Creation of a new instance of $controllerNew using ReflectionClass in order to be able to use various methods on that specific class
+			//More information about the ReflectionClass and it's methods here ->
+				//http://php.net/manual/en/class.reflectionclass.php
+            if ((new \ReflectionClass($controllerNew))->hasMethod($params['act']) && (new \ReflectionMethod($controllerNew, $params['act']))->isPublic()) {
+                call_user_func(array($controllerNew, $params['act']));
+			} else {
+                call_user_func(array($controllerNew, 'notFound'));
+			}
+        }
+        else {
+            call_user_func(array(new $controllerDef, 'notFound'));
+        }
+    }
+}
