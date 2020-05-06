dc874d9c4cc527c70c9b1153cb3753b8fd3a2ad6
Thato Mmusi <mmusi2010@gmail.com>
2020-05-06 09:38:04 +0200

    Create Autoloader.php

---
+++ app/Autoloader.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BonalaPHP\App;
+
+/**
+ * Class Autoloader
+ * @package BonalaPHP\App
+ */
+class Autoloader {
+	
+    static function init() {
+        // Register the autoloader method
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
+
+    // If you're on a linux based server, please uncomment this function and comment all of the next one :
+    /*
+    static function autoload($class){
+
+        // Remove namespace and backslash
+        $class = str_replace(array(__NAMESPACE__, 'BonalaPHP', '\\'), '/', $class);
+        if (is_file(__DIR__ . '/' . $class . '.php')) {
+            require_once(__DIR__ . '/' . $class . '.php');
+        }
+        if (is_file(ROOT_PATH . $class . '.php')) {
+            require_once(ROOT_PATH . $class . '.php');
+        }
+    }
+    */
+
+    /**
+     * @param string $class Contains the class name to be autoloaded.
+     */
+    static function autoload($class){
+
+        // Remove namespace and backslash
+        $class = str_replace(array(__NAMESPACE__, 'BonalaPHP', DIRECTORY_SEPARATOR), '/', $class);
+		
+        if (is_file(__DIR__ . '/' . $class . '.php')) {	
+            require_once(__DIR__ . '/' . $class . '.php');
+		}
+        if (is_file(ROOT_PATH . $class . '.php')) {
+            require_once(ROOT_PATH . $class . '.php');
+		}
+    }
+
+}
